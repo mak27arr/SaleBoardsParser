e1e5e8716d77587f6c9e8f542017b0ff795fb5d9
makar <mak27arr@gmail.com>
2019-12-02 10:49:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaleBoardsParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
